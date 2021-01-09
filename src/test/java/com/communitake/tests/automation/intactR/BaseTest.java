32549a74e2f3bb4febc3571a60bdbc70d1fb34a3
Nissim <nissim@communitale.com>
2021-01-09 23:13:09 +0200

    Added a base test class

--- src/test/java/com/communitake/tests/automation/intactR/BaseTest.java
+++ src/test/java/com/communitake/tests/automation/intactR/BaseTest.java
@@ -1,46 +1,23 @@
 package com.communitake.tests.automation.intactR;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
-import com.communitake.tests.automation.webpageobject.LoginPage;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 
 
-public abstract class BaseTest {
-	WebDriver driver;
-	//AppiumDriver<MobileElement> mobileDriver;
-	private AppiumDriverLocalService service;
-	protected AppiumDriver<MobileElement> mobileDriver;
-
-
-
-	@BeforeClass
-
-	public void Web() {
-		
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://mydevice.communitake.com/MDPortal/login?customer=en");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		LoginPage loginPage = new LoginPage(driver);
-		loginPage.login("nissim+2000@communitake.com", "na260438");
-	}
+public abstract class BaseTest extends com.communitake.tests.automation.BaseTest {
+	
 	
 	@BeforeSuite
 	public AppiumDriver<MobileElement> mobile() throws MalformedURLException  {
